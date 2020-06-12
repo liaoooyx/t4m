88828d331aaf89f7e79aee6aefdec8c35ca5ae5a
liaoooyx <727687719@.qq.com>
2020-06-12 10:48:24 +0100

    EntityScanner constructs all entities from bottom (class) to top (module).

--- t4m-extractor/src/main/java/com/t4m/extractor/entity/ProjectInfo.java
+++ t4m-extractor/src/main/java/com/t4m/extractor/entity/ProjectInfo.java
@@ -1,9 +1,62 @@
 package com.t4m.extractor.entity;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * Created by Yuxiang Liao on 2020-06-09 14:02.
  */
 public class ProjectInfo {
 
+	private String rootPath;
+	private String projectName;
+
+	private ModuleInfo rootModule;
+	private List<ModuleInfo> moduleList;
+
+	public ProjectInfo() {
+		this.moduleList = new ArrayList<>();
+	}
+
+	public ProjectInfo(List<ModuleInfo> moduleList) {
+		this.moduleList = moduleList;
+	}
+
+	public String getRootPath() {
+		return rootPath;
+	}
+
+	public void setRootPath(String rootPath) {
+		this.rootPath = rootPath;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public ModuleInfo getRootModule() {
+		return rootModule;
+	}
+
+	public void setRootModule(ModuleInfo rootModule) {
+		this.rootModule = rootModule;
+	}
+
+	public List<ModuleInfo> getModuleList() {
+		return moduleList;
+	}
+
+	public void setModuleList(List<ModuleInfo> moduleList) {
+		this.moduleList = moduleList;
+	}
+
+	public void addModuleList(ModuleInfo moduleInfo){
+		moduleList.add(moduleInfo);
+	}
 }
---
+++ t4m-extractor/src/main/java/com/t4m/extractor/scanner/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Scan and extract information from target peoject.
+ * @author Yuxiang
+ */
+package com.t4m.extractor.scanner;
