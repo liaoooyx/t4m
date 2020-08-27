1f51b0462a7b74564d86e39b735f88f45b83f617
liaoooyx <727687719@.qq.com>
2020-08-27 20:23:03 +0800

    Update package desc.

--- t4m-extractor/src/main/java/com/t4m/extractor/entity/package-info.java
+++ t4m-extractor/src/main/java/com/t4m/extractor/entity/package-info.java
@@ -1,5 +1,5 @@
 /**
  * This package is defining the entities that used to store and transfer the information of a target project. The
- * information is extracted by INFORMATION EXTRACTOR and will be used by METRICS CALCULATOR
+ * information is extracted by "scanner" classes and will be used by "metric" classes
  */
 package com.t4m.extractor.entity;
